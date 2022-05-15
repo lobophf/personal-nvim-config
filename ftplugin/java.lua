eba7edcda02103f5207570373ba427ebebe39438
Pedro Lobo <lobophf@gmail.com>
2022-05-14 23:11:10 -0300

    Update java.lua

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,10 +2,17 @@ local MAIN_PATH = os.getenv('HOME') .. '/.local/share/eclipse.jdt.ls/org.eclipse
 local JAR_PATH = MAIN_PATH .. '/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar' 
 local CONFIG_PATH = MAIN_PATH .. '/config_linux'
 
+local function debug_stuff()
+  local util = require('jdtls.util')
+  local dap = require("dap")
+  require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+  --require('jdtls.dap').setup_dap_main_class_configs()
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
 local WORKSPACE_DIR = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
 local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
@@ -25,12 +32,14 @@ local config = {
     '-data', vim.fn.expand('~/.cache/nvim/workspaces_jdtls/') .. WORKSPACE_DIR 
   },
   root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-  init_options = {    
-    bundles = {"/home/nix/.config/nvim/debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.36.0.jar"},    
-  }, 
+  init_options = {
+    bundles = {os.getenv('HOME') .. '/.config/nvim/debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.36.0.jar'},
+  },
   capabilities = capabilities
 }
 
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
+
+debug_stuff()
 require('jdtls').start_or_attach(config)
