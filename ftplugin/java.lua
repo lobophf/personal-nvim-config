ebab9b15b395adb7c4e140d7823740a46ce6d85b
Pedro Lobo <lobophf@gmail.com>
2022-09-21 21:34:03 -0300

    Update java.lua

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,9 @@
 local JAR_PATH =  os.getenv('HOME') .. '/.config/nvim/servers/jdt-language-server/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar'
 local CONFIG_PATH = os.getenv('HOME') .. '/.config/nvim/servers/jdt-language-server/config_linux'
 local DEBUG_PATH = os.getenv('HOME') .. '/.config/nvim/debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.36.0.jar'
+-- local DEBUG_TEST_PATH = os.getenv('HOME') ..'/.config/nvim/vscode-java-test/server/com.microsoft.java.test.plugin-0.37.1.jar'
+local DEBUG_TEST_PATH = os.getenv('HOME') ..'/.config/nvim/vscode-java-test/server/*.jar'
+
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
@@ -21,6 +24,12 @@ dap.adapters.java = function(callback)
   end)
 end
 
+-- Debugging
+local bundles = {
+  vim.fn.glob( DEBUG_PATH ),
+}
+vim.list_extend(bundles, vim.split(vim.fn.glob( DEBUG_TEST_PATH ), "\n"))
+
 local config = {
   cmd = {
     'java',
@@ -39,7 +48,7 @@ local config = {
   },
   root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
   init_options = {
-    bundles = { DEBUG_PATH },
+      bundles = bundles,
   },
   capabilities = capabilities,
   on_attach = function(client, bufnr) 
