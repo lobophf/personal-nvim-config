d6456da7f9a6a366fbebc6566d304ee533f1bc29
Pedro Lobo <lobophf@gmail.com>
2022-11-06 19:17:15 -0300

    Update keybindinds.lua

--- lua/keybindinds.lua
+++ lua/keybindinds.lua
@@ -1,4 +1,5 @@
 local keymap = vim.api.nvim_set_keymap
+-- JAVA_LSP
 keymap('n', 'gp',':e#<CR>',{})
 keymap('n', 'gt', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
 keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
@@ -13,7 +14,28 @@ keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
 keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {})
 keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', {})
 keymap('n', '<leader>e', '<cmd>:Lex 30<CR>', {})
+
+-- Numbers && Relative Numbers
 keymap('n', '<leader>n', '<cmd>:set nonumber! | :set norelativenumber!<CR>', {})
+
+-- Telescope
 keymap('n', '<tab>f', '<cmd>:Telescope find_files<CR>', {})
+
+-- Copy & Paste from / to clipboard
 keymap('v', '<leader>y', '"+y', {})
 keymap('n', '<leader>p', '"+p', {})
+
+-- Files Tree
+keymap('n', '\'', '<cmd>:NvimTreeToggle<CR>', {})
+keymap('n', '<BSlash>z', '<cmd>:NvimTreeCollapse<CR>', {})
+
+-- Debug
+keymap('n', '<F1>', [[<cmd>lua require'dap'.continue()<CR>]], {})
+keymap('n', '<F2>', [[<cmd>:lua require'dap'.step_over()<CR>]], {})
+keymap('n', '<F3>', [[<cmd>:lua require'dap'.step_into()<CR>]], {})
+keymap('n', '<F4>', [[<cmd>:lua require'dap'.step_out()<CR>]], {})
+keymap('n', '<Leader>b', [[<cmd>lua require'dap'.toggle_breakpoint()<CR>]], {})
+keymap('n', '<Leader>cb', [[<cmd>:lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>]], {})
+keymap('n', '<Leader>lp', [[<cmd>:lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '<CR>]], {})
+keymap('n', '<Leader>dr', [[<cmd>:lua require'dap'.repl.open()<CR>]], {})
+keymap('n', '<Leader>rl', [[<cmd>:lua require'dap'.run_last()<CR>]], {})
