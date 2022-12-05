ad9a1b77658158a45c0f01d7c2fc9c7447123585
Pedro Lobo <lobophf@gmail.com>
2022-12-05 17:35:35 -0300

    Update config.lua

--- lua/config.lua
+++ lua/config.lua
@@ -1,10 +1,16 @@
-vim.bo.expandtab = true
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
-vim.bo.tabstop = 2
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.g.bufferline_echo = 0
-vim.g.shortmess = at
-vim.g.cmdheight = 2
-vim.bo.modifiable = true
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("me", {clear = true}),
+--  pattern = "*.java",
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.expandtab = true
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+    vim.g.bufferline_echo = 0
+    vim.g.shortmess = at
+    vim.g.cmdheight = 2
+    vim.bo.modifiable = true
+  end,
+})
